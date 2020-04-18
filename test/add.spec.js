091cd11f4ed4a9974d8519417b9cc74b386f036f
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-18 13:12:22 -0500

    [Feature] Created files to testing and included assertion

---
+++ test/add.spec.js
@@ -0,0 +1,8 @@
+const add = require('../add');
+const assert = require('assert');
+
+const result = add(1, 3);
+
+const expected = 4;
+
+assert.equal(result, expected, 'Add function does not work properly.');
