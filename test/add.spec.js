159bb4ee86d3a0442f6be217978477d161abfc51
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-18 13:18:59 -0500

    [Update] Refactored code to implement a pretty message in the terminal

--- test/add.spec.js
+++ test/add.spec.js
@@ -1,8 +1,11 @@
 const add = require('../add');
 const assert = require('assert');
 
-const result = add(1, 3);
+describe('The add function', () => {
+  it('adds two numbers', () => {
+    const result = add(1, 3);
+    const expected = 4;
 
-const expected = 4;
-
-assert.equal(result, expected, 'Add function does not work properly.');
+    assert.equal(result, expected);
+  });
+});
