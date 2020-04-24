b25f49d414411b577d7b96ad79814a863a093e0b
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-23 20:11:49 -0500

    [Update] Added confirm email test to user

--- auth-app/__tests__/server/models/user.spec.js
+++ auth-app/__tests__/server/models/user.spec.js
@@ -10,15 +10,44 @@ import bcrypt from 'bcryptjs'
 const app = () => supertest(server)
 
 describe('The user model', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+    })
+
     it('should hash the user password before saving to the database', async () => {
         const user = {
             name: 'Test User',
             email: 'test@user.com',
             password: 'password'
         }
+
         const createdUser = await User.create(user)
         expect(bcrypt.compareSync(user.password, createdUser.password)).toBe(
             true
         )
+    })
+
+    it('should hash the user password before saving the database', async () => {
+        const user = {
+            name: 'Test User',
+            email: 'test@user.com',
+            password: 'password'
+        }
+
+        const createdUser = await User.create(user)
+        expect(bcrypt.compareSync(user.password, createdUser.password)).toBe(
+            true
+        )
+    })
+
+    it('should set the email confirm code for the user before saving to the database', async () => {
+        const user = {
+            name: 'Test User',
+            email: 'test@user.com',
+            password: 'password'
+        }
+
+        const createdUser = await User.create(user)
+        expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
     })
 })
