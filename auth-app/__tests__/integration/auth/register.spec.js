6dfc8ac2b3d18da3cb71eef41f996d1959ff8298
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-30 19:24:30 -0500

    [Feature] Created error testing in authentication when email has already been taken

--- auth-app/__tests__/integration/auth/register.spec.js
+++ auth-app/__tests__/integration/auth/register.spec.js
@@ -5,19 +5,34 @@ import server from '@server/app'
 import supertest from 'supertest'
 import { disconnect } from '@tests/utils/mongoose'
 
+import User from '@models/User'
+
 const app = () => supertest(server)
 
 describe('The register process', () => {
+    let user = {
+        name: 'Test User',
+        email: 'test01@user.com',
+        password: 'password'
+    }
+
     it('should register a new user', async () => {
         const response = await app()
             .post('/api/v1/auth/register')
-            .send({
-                name: 'Test User',
-                email: 'test01@user.com',
-                password: 'password'
-            })
+            .send(user)
+    })
 
-        console.log(response.body)
+    it('should return a 422 if registration fails', async () => {
+        await User.create(user)
+        const response = await app()
+            .post('/api/v1/auth/register')
+            .send(user)
+
+        expect(response.status).toBe(422)
+        expect(response.body.message).toBe('Validation failed.')
+        expect(response.body.data.errors).toEqual({
+            email: 'This email has already been taken.'
+        })
     })
 
     afterAll(async () => {
